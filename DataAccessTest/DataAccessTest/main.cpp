7e04c77de8104528241b6cd69fe6ffa7c93711b1
Alexander Rummel <rummel.alexander@googlemail.com>
2013-12-03 10:32:32 +0100

    Initial Commit

---
+++ DataAccessTest/DataAccessTest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  DataAccessTest
+//
+//  Created by Alexander Rummel on 03.12.13.
+//  Copyright (c) 2013 Alexander Rummel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
